caa72e92c6ffd77373db2942c0d4fca327e75b10
Rahul-Encoded <rahulm03@outlook.com>
2025-03-14 02:55:41 +0530

    feat: add routing for book management and create page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateBooks from './pages/CreateBooks'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBooks from './pages/ShowBooks'
 
 function App() {
   return (
-    <div className=''>App</div>
+    <Routes>
+      <Route path='/' element={<Home></Home>} />
+      <Route path='/books/create' element={<CreateBooks></CreateBooks>} />
+      <Route path='/books/details/:id' element={<ShowBooks></ShowBooks>} />
+      <Route path='/books/edit/:id' element={<EditBook></EditBook>} />
+      <Route path='/books/delete/:id' element={<DeleteBook></DeleteBook>} />
+    </Routes>
   )
 }
 
