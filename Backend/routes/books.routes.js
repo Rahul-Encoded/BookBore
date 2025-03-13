dda6009bb74577c6702aa4669d5dbb2c96801979
Rahul-Encoded <rahulm03@outlook.com>
2025-03-14 02:04:20 +0530

    feat: enhance book routes with CRUD operations

--- Backend/routes/books.routes.js
+++ Backend/routes/books.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { addBook } from "../controllers/books.controllers.js";
+import { addBook, deleteBook, getBook, getBooks, updateBook } from "../controllers/books.controllers.js";
 
 const router = Router();
 
-router.route("/addbook").post(addBook);
+//Route to add a new book
+router.route("/").post(addBook);
+
+//Route to fetch all books
+router.route("/").get(getBooks);
+
+//Route to fetch all books
+router.route("/:id").get(getBook);
+
+//Route to update a book
+router.route("/:id").put(updateBook);
+
+//Route to delete a book
+router.route("/:id").delete(deleteBook);
 
 export default router;
